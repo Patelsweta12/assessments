b51d658c0930ebacc542c32379a4a9b5c1032074
Patelsweta12 <swetapatel11114@gmail.com>
2023-03-30 17:48:00 +0530

    search pipe

---
+++ routingAndServicewithFilter/servicesWithRouting/service-and-routing/src/app/student/search.pipe.ts
@@ -0,0 +1,16 @@
+
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+
+  transform(value:any,searchTerm: any): any {
+
+    if(!searchTerm){
+      return value
+    }
+    return value.filter((search: any) => search.name.toLowerCase().indexOf(searchTerm.toLowerCase())> -1 ||  search.id.toString().indexOf(searchTerm)> -1 || search.address.toString().indexOf(searchTerm)> -1 || search.age.toString().indexOf(searchTerm)> -1 || search.address.toLowerCase().indexOf(searchTerm.toLowerCase())> -1  )
+  }
+}
